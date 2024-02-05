e5766fa0ca7653b0d95f41035f62f1e6fc06d1a8
Serhiy Stetskovych <serhiy.stetskovych@connectwise.com>
2024-02-05 22:39:01 +0200

    Comment code

--- pflow/text/cleaners.py
+++ pflow/text/cleaners.py
@@ -1,19 +1,19 @@
-from text.textnormalizer import norm
-from ukrainian_word_stress import Stressifier
-import regex
-import re
-from ipa_uk import ipa
-stressify = Stressifier()
+# from text.textnormalizer import norm
+# from ukrainian_word_stress import Stressifier
+# import regex
+# import re
+# from ipa_uk import ipa
+# stressify = Stressifier()
 
 
-_whitespace_re = re.compile(r"\s+")
-def collapse_whitespace(text):
-    return re.sub(_whitespace_re, " ", text)
+# _whitespace_re = re.compile(r"\s+")
+# def collapse_whitespace(text):
+#     return re.sub(_whitespace_re, " ", text)
 
 
-def ukr_cleaners(text):
-    text = collapse_whitespace(text)
-    text = norm(text).lower()
+# def ukr_cleaners(text):
+#     text = collapse_whitespace(text)
+#     text = norm(text).lower()
 
-    text = regex.sub(r'[^\p{L}\p{N}\?\!\,\.\-\: ]', '', text)
-    return ipa(stressify(text), False)
+#     text = regex.sub(r'[^\p{L}\p{N}\?\!\,\.\-\: ]', '', text)
+#     return ipa(stressify(text), False)
